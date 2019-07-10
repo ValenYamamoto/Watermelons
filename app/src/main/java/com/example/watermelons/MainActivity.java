be715dfa99f6cbd9c3f0ce24eec3104e4951a926
Valen Yamamoto <valenryamamoto@gmail.com>
2019-07-09 23:45:33 -0700

    Changed to AudioRecorder, sending ERROR_BAD_VALUE

--- app/src/main/java/com/example/watermelons/MainActivity.java
+++ app/src/main/java/com/example/watermelons/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -18,6 +21,7 @@ import android.widget.LinearLayout;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 
@@ -30,6 +34,25 @@ public class MainActivity extends AppCompatActivity {
 
     private RecordButton recordButton = null;
     private MediaRecorder recorder = null;
+    private AudioRecord audioRecorder = null;
+
+    private int audioSource = MediaRecorder.AudioSource.MIC;
+    private int samplingRate = 48000; // Hz
+    private int channelConfig = AudioFormat.CHANNEL_IN_MONO;
+    private int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    private int bufferSize = 192; //AudioRecord.getMinBufferSize(samplingRate, channelConfig, audioFormat);
+    private int sampleNumBits = 16;
+    private int numChannels = 1;
+
+    private double recordTime = 1;
+    private long startTime = 0;
+
+    private int currentIndex = 0;
+
+    private double[] dataArray = null;
+    private double[] timePoints = null;
+
+    private boolean isRecording = false;
 
     private PlayButton playButton = null;
     private MediaPlayer player = null;
@@ -81,26 +104,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startRecording() {
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setOutputFile(fileName  + new SimpleDateFormat("MMddhhmmss").format(new Date()) + ".wav");
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            recorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-        recorder.start();
+        audioRecorder.startRecording();
+        isRecording = true;
+        startTime = System.currentTimeMillis();
     }
 
     private void stopRecording() {
-        recorder.stop();
-        recorder.release();
-        recorder = null;
+        audioRecorder.stop();
+        audioRecorder = null;
+        isRecording = false;
+        currentIndex = 0;
+        for(int i = 0; i < dataArray.length; i ++) {
+            Log.d("Printing Data", "Data Array -> " + dataArray[i]);
+        }
+        Log.d("While Reocrding", "Before DFT");
+        dft(timePoints, dataArray);
     }
+
+    private int read() {
+        byte[] data = new byte[8];
+        int readBytes = audioRecorder.read(data, 0, bufferSize);
+        for(int i = 0; i < data.length; i ++) {
+            Log.d("Reading Data", "Bytes -> " + data);
+        }
+        return readBytes;
+    }
+
+    private void dft(double[] inreal, double[] inimag) {
+        int n = inreal.length;
+
+        double[] outreal = new double[n];
+        double[] outimag = new double[n];
+
+//        Log.d("Inside DFT", "Before Loop: " + n);
+        for(int k = 0; k < n; k++) {
+            double sumreal = 0;
+            double sumimag = 0;
+
+            for (int t = 0; t < n; t++) {
+                //Log.d("Inside DFT", "First For Loop: " + t);
+                double angle = 2 * Math.PI * t * k / n;
+                sumreal += inreal[t] * Math.cos(angle) + inimag[t] * Math.sin(angle);
+                sumimag += -inreal[t] * Math.sin(angle) + inimag[t] * Math.cos(angle);
+            }
+            outreal[k] = sumreal;
+            outimag[k] = sumimag;
+//            Log.d("Inside DFT", "Firsrst For Loop: " + k + "   e   " + sumreal + "   " + sumimag);
+
+        }
+        Log.d("Inside DFT", "Finished");
+    }
+
 
     class RecordButton extends AppCompatButton {
         boolean mStartRecording = true;
@@ -151,7 +205,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.d("App Up", "Staring");
+        AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        Log.d("Initializing", "MinBufferSize -> " + bufferSize);
+        audioRecorder = new AudioRecord(audioSource, samplingRate, channelConfig, audioFormat, bufferSize);
         fileName = getExternalCacheDir().getAbsolutePath();
         fileName += "/watermelons";
 
@@ -171,6 +228,35 @@ public class MainActivity extends AppCompatActivity {
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                 0));
         setContentView(ll);
+
+        dataArray = new double[samplingRate * (int) recordTime];
+        timePoints = new double[dataArray.length];
+        for(int i = 0; i < timePoints.length; i++) {
+            timePoints[i] = (double) i/ samplingRate;
+        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    while (isRecording) {
+                        Log.d("Thread", "isRecording");
+                        try {
+                            if (System.currentTimeMillis() < startTime + recordTime * 1000) {
+                                dataArray[currentIndex] = read();
+                                currentIndex++;
+                            } else {
+                                stopRecording();
+                                Log.d("While Reocrding", "Out of Time");
+                            }
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            stopRecording();
+                            Log.d("While Reocrding", "Array Index Out of Bounds");
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
